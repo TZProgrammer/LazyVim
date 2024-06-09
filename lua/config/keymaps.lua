e1b4f37df60d4ce3a3abdae0342648ce2f38464b
TZProgrammer <thomaszeger@gmail.com>
2024-06-09 01:20:32 -0400

    keymaps: remove broken keymaps for neogen cycling

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,7 +1,3 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-
-local opts = { noremap = true, silent = true }
-vim.api.nvim_set_keymap("i", "<A-l>", "<Esc>:lua require('neogen').jump_next<CR>", opts)
-vim.api.nvim_set_keymap("i", "<A-h>", "<Esc>:lua require('neogen').jump_prev<CR>", opts)
