d08c6d61420f2bcb6b1209a3ee90fd2914ca1755
Thomas Zeger <thomaszeger@gmail.com>
2024-08-22 16:27:01 -0400

    add test code. WIP

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -8,5 +8,16 @@ return {
       end,
       desc = "Grep Files (Root Dir)"
     },
+--  { "<leader>sf", ":lua require('telescope').extensions.live_grep_args.live_grep_args( { postfix = '-u' } )<CR>", desc = "Live Grep All (Root Dir)" },
   },
 }
+--    {
+--      event = "VeryLazy",
+--      "nvim-telescope/telescope-live-grep-args.nvim",
+--      config = function(_, _)
+--        require("lazyvim.util").on_load("telescope.nvim", function()
+--          require("telescope").load_extension("live_grep_args")
+--        end)
+--      end,
+--    },
+--  },
