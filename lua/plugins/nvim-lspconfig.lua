639ab3c013ed8fc6f104f9eecf09bb44a6dc649e
TZProgrammer <thomaszeger@gmail.com>
2025-01-11 20:48:00 -0500

    inform c++ lsp of version 23

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -3,6 +3,14 @@ return {
     "neovim/nvim-lspconfig",
     opts = {
       autoformat = false,
+      servers = {
+        clangd = {
+          cmd = { "clangd", "--clang-tidy", "--completion-style=detailed", "--header-insertion=never" },
+          init_options = {
+            fallbackFlags = { "--std=c++23" },
+          },
+        },
+      },
     },
   },
 }
